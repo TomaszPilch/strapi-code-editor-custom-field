e4530f6f79c3b7cd1b36251a6ca2c36efaac9884
Tomasz Pilch <55186528+TomaszPilch@users.noreply.github.com>
2025-01-25 20:34:01 +0100

    TECH - rewrite plugin to strapi v5

--- admin/src/components/CodeEditor/CodeEditor.tsx
+++ admin/src/components/CodeEditor/CodeEditor.tsx
@@ -1,12 +1,10 @@
 import React, { Suspense, useState, useEffect } from 'react'
-import { useIntl } from 'react-intl'
 import { debounceTime, Subject } from 'rxjs'
 import styled from 'styled-components'
 import CodeEditorLib, { EditorProps } from '@monaco-editor/react'
 
 import { Flex, Loader, SingleSelect, SingleSelectOption, IconButton, Field } from '@strapi/design-system'
 import { Expand } from '@strapi/icons'
-import { MessageFormatElement } from '@formatjs/icu-messageformat-parser'
 
 import initWorkers from '../../utils/workers'
 
@@ -48,26 +46,21 @@ const NormalDiv = styled.div`
   min-height: 35vh;
 `
 
-interface MessageDescriptor {
-  id?: string
-  description?: string | object
-  defaultMessage?: string | MessageFormatElement[]
-}
-
 type CodeEditorPropsT = {
   attribute: {
+    type: string
     customField: string
     options?: {
       language?: string
       defaultValue?: string
     }
   }
-  description: MessageDescriptor
+  description: string
   error?: string
-  intlLabel: MessageDescriptor
+  label: string
   labelAction?: React.ReactNode
   name: string
-  onChange: (ev: { target: { name: string; value: string } }) => void
+  onChange: (ev: { target: { name: string; type: string; value: any } }) => void
   required: boolean
   value: null | string
 }
@@ -79,26 +72,29 @@ const CodeEditor = ({
   description,
   // disabled, - todo readonly
   error,
-  intlLabel,
+  label,
   labelAction,
   name,
   onChange,
   required,
   value,
 }: CodeEditorPropsT) => {
-  const { formatMessage } = useIntl()
   const languageRegExp = new RegExp('__(.+)__;')
   const isJson = attribute.customField.endsWith('code-editor-json')
   const languageFromOptions = attribute?.options?.language
   const defaultLanguage = isJson ? 'json' : 'javascript'
-  let languageFromValue: null | Array<string> | string = value ? value.match(languageRegExp) : null
+  if (isJson && value && typeof value === 'object') {
+    value = JSON.stringify(value, null, 2)
+  }
+  const hasValue = typeof value === 'string'
+  let languageFromValue: null | Array<string> | string = hasValue && value ? value.match(languageRegExp) : null
   if (languageFromValue && languageFromValue.length > 1) {
     languageFromValue = languageFromValue[1] as string
   } else {
     languageFromValue = null
   }
   const defaultValue = attribute?.options?.defaultValue || undefined
-  let defaultValueForEditor = value ? value.replace(languageRegExp, '') : ''
+  let defaultValueForEditor = hasValue && value ? value.replace(languageRegExp, '') : ''
   if (
     (!defaultValueForEditor && defaultValue) ||
     (isJson && typeof value === 'object' && value && Object.keys(value).length === 0 && defaultValue)
@@ -113,14 +109,14 @@ const CodeEditor = ({
 
   if (prevValue !== value) {
     setPrevValue(value)
-    setEditorValue(value ? value.replace(languageRegExp, '') : '')
+    setEditorValue(hasValue && value ? value.replace(languageRegExp, '') : '')
   }
 
   // @ts-ignore
   const theme = localStorage.getItem('STRAPI_THEME')
 
   const handleOnChange = (value: string) => {
-    onChange({ target: { name, value } })
+    onChange({ target: { name, type: attribute.type, value } })
     setPrevValue(value)
   }
 
@@ -143,16 +139,18 @@ const CodeEditor = ({
   const StyledDiv = fullScreen ? FullScreenDiv : NormalDiv
   const Editor = CodeEditorLib as unknown as React.FC<EditorProps>
   return (
-    <Field name={name} id={name} error={error} hint={description && formatMessage(description)} required={required}>
+    <Field.Root name={name} id={name} error={error} hint={description} required={required}>
       <StyledDiv>
-        <Flex spacing={3}>
-          <Flex>
+        <Flex gap={3} direction="column">
+          <Flex width="100%">
             <Flex width="30%" justifyContent="flex-start">
-              <Field.Label action={labelAction}>{formatMessage(intlLabel)}</Field.Label>
+              <Field.Label action={labelAction}>{label}</Field.Label>
             </Flex>
             <Flex width="70%" justifyContent="flex-end">
-              <Flex spacing={2} direction="column">
-                <IconButton onClick={() => setFullScreen((prev) => !prev)} label="expand" icon={<Expand />} />
+              <Flex gap={2}>
+                <IconButton onClick={() => setFullScreen((prev) => !prev)} label="expand">
+                  <Expand />
+                </IconButton>
                 {!isJson && !languageFromOptions && (
                   <SingleSelect id={`${name}-language-select`} label="" value={language} onChange={setLanguage}>
                     {languages.map((lang) => (
@@ -180,7 +178,7 @@ const CodeEditor = ({
           <Field.Error />
         </Flex>
       </StyledDiv>
-    </Field>
+    </Field.Root>
   )
 }
 
